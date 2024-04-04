75ad986ecf91851e500e435114b8bd0cdfd9c0aa
givigelashvili <55489231+aquarazorda@users.noreply.github.com>
2024-04-04 16:10:13 +0400

    feat: basic routing

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,51 @@
 import { renderToReadableStream } from "react-dom/server";
 import * as Http from "@effect/platform/HttpServer";
 import * as BunServer from "@effect/platform-bun/Http/Server";
-import { Layer, Effect, Stream, pipe, Chunk, flow } from "effect";
+import { Layer, Effect, Stream, pipe, Chunk, flow, Logger } from "effect";
 import { BunRuntime } from "@effect/platform-bun";
-import StreamingComponent from "./component";
+import { lazy } from "react";
 
 const ServerLive = BunServer.layer({
   port: 3000,
 });
 
-const mainRoute = Http.router.get(
-  "/",
+class ReactError {
+  readonly _tag = "ReactError";
+  message: string = "";
+  constructor(message?: string) {
+    if (message) {
+      this.message = message;
+    }
+  }
+}
+
+const renderFromUrl = (response: Http.request.ServerRequest) =>
   Effect.gen(function* (_) {
+    const path = response.url.split("/");
+    const filePath = `${import.meta.dir}/routes/${path
+      .filter((s) => s)
+      .join("/")}/page.tsx`;
+
+    if (!Bun.file(filePath).size) {
+      return Http.response.text("404", { status: 404 });
+    }
+
+    const Component = lazy(() => import(filePath));
+
     const componentReadable = yield* _(
-      Effect.tryPromise(() => renderToReadableStream(<StreamingComponent />)),
-    );
-    const componentStream = Stream.fromReadableStream(
-      () => componentReadable,
-      () => "",
+      pipe(
+        Effect.tryPromise(() => renderToReadableStream(<Component />)),
+        Effect.map((c) =>
+          Stream.fromReadableStream(
+            () => c,
+            () => undefined,
+          ),
+        ),
+        Effect.flatMap(Effect.fromNullable),
+        Effect.mapError(
+          () => new ReactError("Route is not exporting default function."),
+        ),
+      ),
     );
 
     return Http.response.stream(
@@ -25,7 +53,7 @@ const mainRoute = Http.router.get(
         Stream.make(
           "<html><head><title>Page</title></head><body><div id='root'>",
         ),
-        Stream.concat(componentStream),
+        Stream.concat(componentReadable),
         Stream.concat(Stream.make("</div></body></html>")),
       ),
       {
@@ -33,11 +61,17 @@ const mainRoute = Http.router.get(
         contentType: "text/html",
       },
     );
-  }),
-);
+  });
 
 const serve = Http.router.empty.pipe(
-  mainRoute,
+  Http.router.route("*")(
+    "*",
+    pipe(
+      Http.request.ServerRequest,
+      Effect.tap(Effect.logInfo),
+      Effect.flatMap(renderFromUrl),
+    ),
+  ),
   Http.server.serve(),
   Layer.provide(ServerLive),
 );
